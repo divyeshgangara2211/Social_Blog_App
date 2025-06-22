a32c32cf0e1eeed759068f18c78888cb1511d59b
Divyesh Gangera <divyeshgangera22@gmail.com>
2025-06-22 22:09:32 +0530

    Implement AuthService for user authentication with Appwrite

---
+++ src/services/auth.js
@@ -0,0 +1,74 @@
+import conf from '../conf/conf.js';
+import { Client,Account ,ID } from 'appwrite';
+
+export class AuthService {
+    client = new Client();
+    account ; 
+
+    constructor(){
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        
+        this.account = new Account(this.client);
+
+    }
+
+    async createAccount( {email, password, name }){
+        try {
+            const response = await this.account.create(
+                ID.unique(),
+                email,
+                password,
+                name    
+            );
+            
+            if( response ){
+                // call another method 
+                return this.login({ email , password });
+            }else{
+                return response;
+            }
+
+        } catch (error) {
+            console.error('Error creating account:', error);
+            throw error;
+        }   
+    }
+
+    async login( { email , password}){
+        try{
+            return await this.account.createEmailPasswordSession(email, password);
+            
+        }catch(error){
+            console.error('Error logging in:', error);
+            throw error;
+        }
+    }
+
+    async  getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.error('Error getting current user:', error);
+            // throw error;
+        }
+
+        return null;
+    }
+
+    async logout(){
+        try{
+            // return await this.account.deleteSession('current');  for single session
+            return await this.account.deleteSessions(); // for all sessions
+        }catch(error){
+            console.error('Error logging out:', error);
+            throw error;
+        }
+    } 
+
+}
+
+const authService = new AuthService();
+
+export default authService;
