493ca7b28757a8b1ff4dbe20e6e1ae122b89f8da
Divyesh Gangera <divyeshgangera22@gmail.com>
2025-06-23 00:49:37 +0530

    feat: integrate Tailwind CSS and Redux for user authentication
    - Added Tailwind CSS and PostCSS dependencies in package.json
    - Created postcss.config.js for Tailwind CSS and autoprefixer configuration
    - Updated App component to manage user authentication state using Redux
    - Created index.jsx to export Header and Footer components
    - Added Tailwind CSS directives in index.css
    - Wrapped App component with Redux Provider in main.jsx
    - Created tailwind.config.js for Tailwind CSS configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,45 @@
-import './App.css'
+import './App.css';
+import React, { useState, useEffect, use } from 'react';
+import { useDispatch } from 'react-redux';
+import authService from './services/auth.js';
+import { login ,logout } from './store/authSlice.js';
+import { Header , Footer } from './components/index.jsx';
+import { Outlet } from 'react-router-dom';
+
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1>A Blog App with Appwrite</h1>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then( (userData) => {
+        if(userData){
+          dispatch(login(userData));
+        }else{
+          dispatch(logout());
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching current user:', error);
+        dispatch(logout());
+      })
+      .finally( () => setLoading(false));
+
+  }, []);
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-black'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null ;
+  
 }
 
 export default App
