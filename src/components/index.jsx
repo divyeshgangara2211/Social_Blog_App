c47d4ac0dcd55376f31b22b160283898c8b9ce1b
Divyesh Gangera <divyeshgangera22@gmail.com>
2025-06-23 23:21:26 +0530

    feat: add reusable Button, Input, and Logo components; enhance Header and Footer with navigation and layout

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,7 +1,11 @@
 import Header from "./Header/Header.jsx";
 import Footer from "./Footer/Footer.jsx";
+import Container from "./Container/Container.jsx";
+import Logo from "./logo.jsx";
 
 export { 
     Header, 
-    Footer 
+    Footer,
+    Container,
+    Logo
 };
